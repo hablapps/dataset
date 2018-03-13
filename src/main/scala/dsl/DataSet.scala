8f5f2aa2e685f6791611736d2eab2690df71cd51
Juan Manuel Serrano <juanmanuel.serrano@hablapps.com>
2018-03-13 09:51:34 +0100

    Relocate implicits

--- src/main/scala/dsl/DataSet.scala
+++ src/main/scala/dsl/DataSet.scala
@@ -10,68 +10,53 @@ abstract class DataSet[A]{
   }
 }
 
-object DataSet extends DataSetSyntax
-
 case class Expand[A,B,D<:DataSet[A]](
   f: D, g: A => TraversableOnce[B]) extends DataSet[B]
-
-object Expand{
-  implicit def toUnapply[A,B,D<:DataSet[A]] =
-    Unapply[B,DataSet,Expand[A,B,D]]
-}
 
 case class Filter[A,D<:DataSet[A]](
   f: D, g: A => Boolean) extends DataSet[A]
 
-object Filter{
-  implicit def toUnapply[A,D<:DataSet[A]] =
-    Unapply[A,DataSet,Filter[A,D]]
-}
-
 case class DMap[A,B,D<:DataSet[A]](
   f: D, g: A => B) extends DataSet[B]
-
-object DMap{
-  implicit def toUnapply[A,B,D<:DataSet[A]] =
-    Unapply[B,DataSet,DMap[A,B,D]]
-}
 
 case class MapValues[A,B,C,D<:DataSet[(A,B)]](
   f: D, g: B => C) extends DataSet[(A,C)]
 
-object MapValues{
-  implicit def toUnapply[A,B,C,D<:DataSet[(A,B)]] =
-    Unapply[(A,C),DataSet,MapValues[A,B,C,D]]
-}
-
 case class ReduceByKey[A,B,D<:DataSet[(A,B)]](
   f: D, g: (B,B)=>B) extends DataSet[(A,B)]
 
-object ReduceByKey{
-  implicit def toUnapply[A,B,D<:DataSet[(A,B)]] =
-    Unapply[(A,B),DataSet,ReduceByKey[A,B,D]]
-}
-
 case class GroupByKey[A,B,D<:DataSet[(A,B)]](
   f: D) extends DataSet[(A,Seq[B])]
-
-object GroupByKey{
-  implicit def toUnapply[A,B,D<:DataSet[(A,B)]] =
-    Unapply[(A,Seq[B]),DataSet,GroupByKey[A,B,D]]
-}
 
 case class SortBy[A,B,D<:DataSet[A]](
   f: D, g: A => B, asc: Boolean = false)(implicit O: Ordering[B]) extends DataSet[A]{
   val ord: Ordering[B] = O
 }
 
-object SortBy{
-  implicit def toUnapply[A,B,D<:DataSet[A]] =
-    Unapply[A,DataSet,SortBy[A,B,D]]
-}
-
 case class Source(content: List[String]) extends DataSet[String]
 
-object Source{
-  implicit val toUnapply = Unapply[String,DataSet,Source]
+object DataSet extends DataSetSyntax{
+  implicit val toUnapplySource = 
+    Unapply[String,DataSet,Source]
+
+  implicit def toUnapplySortBy[A,B,D<:DataSet[A]] =
+    Unapply[A,DataSet,SortBy[A,B,D]]
+
+  implicit def toUnapplyGroupByKey[A,B,D<:DataSet[(A,B)]] =
+    Unapply[(A,Seq[B]),DataSet,GroupByKey[A,B,D]]
+
+  implicit def toUnapplyReduceyKey[A,B,D<:DataSet[(A,B)]] =
+    Unapply[(A,B),DataSet,ReduceByKey[A,B,D]]
+
+  implicit def toUnapplyMapValues[A,B,C,D<:DataSet[(A,B)]] =
+    Unapply[(A,C),DataSet,MapValues[A,B,C,D]]
+
+  implicit def toUnapplyFilter[A,D<:DataSet[A]] =
+    Unapply[A,DataSet,Filter[A,D]]
+
+  implicit def toUnapplyExpand[A,B,D<:DataSet[A]] =
+    Unapply[B,DataSet,Expand[A,B,D]]
+
+  implicit def toUnapplyDMap[A,B,D<:DataSet[A]] =
+    Unapply[B,DataSet,DMap[A,B,D]]
 }
